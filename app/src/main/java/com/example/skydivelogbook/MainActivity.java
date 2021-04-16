683136e8f8a049c6473e0c0848bc7213333a9e25
someone <someone@someplace.com>
2021-04-15 22:21:13 -0400

    4/15/21_2

--- app/src/main/java/com/example/skydivelogbook/MainActivity.java
+++ app/src/main/java/com/example/skydivelogbook/MainActivity.java
@@ -4,18 +4,56 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.Toolbar;
 
 import com.example.skydivelogbook.db.LogDatabase;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Menu optionsMenu;
+    private boolean edit = true;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        setSupportActionBar(findViewById(R.id.toolbar));
+    }
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        optionsMenu = menu;
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.activity_main, menu);
+
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.menuEdit:
+                if (edit) {
+                    edit = false;
+                    item.setIcon(R.drawable.save_icon);
+                    findViewById(R.id.upsaNumberDisplay).setEnabled(true);
+                    findViewById(R.id.uspaLicenseDisplay).setEnabled(true);
+                }
+                else{
+                    edit = true;
+                    item.setIcon(R.drawable.edit_icon);
+                    findViewById(R.id.upsaNumberDisplay).setEnabled(false);
+                    findViewById(R.id.uspaLicenseDisplay).setEnabled(false);
+                }
+                return true;
+            default: return true;
+        }
     }
 
     public void add(View view) {
